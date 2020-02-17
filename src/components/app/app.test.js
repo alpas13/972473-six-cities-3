503b3df1a47e82360639ba2e76952cc8479eb02c
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-02-17 14:26:41 +0100

    Fixed and updated snapshot tests for main and app component

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,17 +2,69 @@ import React from "react";
 import rerender from "react-test-renderer";
 import App from "./app";
 
-const titlesOffers = [
-  `Beautiful & luxurious apartment at great location`,
-  `Wood and stone place`,
-  `Canal View Prinsengracht`,
-  `Nice, cozy, warm big bed apartment`,
-  `Wood and stone place`
-];
+const mock = {
+  offers: [
+    {
+      id: Date.now() + Math.random(),
+      title: `Title text 1`,
+      mark: `mark 1`,
+      previewImage: `path 1`,
+      price: 1,
+      bookmark: false,
+      rating: 1,
+      type: `type text 1`
+    },
+    {
+      id: Date.now() + Math.random(),
+      title: `Title text 2`,
+      mark: `mark 2`,
+      previewImage: `path 2`,
+      price: 2,
+      bookmark: true,
+      rating: 2,
+      type: `type text 2`
+    },
+    {
+      id: Date.now() + Math.random(),
+      title: `Title text 3`,
+      mark: `mark 3`,
+      previewImage: `path 3`,
+      price: 3,
+      bookmark: false,
+      rating: 3,
+      type: `type text 3`
+    },
+    {
+      id: Date.now() + Math.random(),
+      title: `Title text 4`,
+      mark: `mark 4`,
+      previewImage: `path 4`,
+      price: 4,
+      bookmark: false,
+      rating: 4,
+      type: `type text 4`
+    },
+    {
+      id: Date.now() + Math.random(),
+      title: `Title text 5`,
+      mark: `mark 5`,
+      previewImage: `path 5`,
+      price: 5,
+      bookmark: true,
+      rating: 5,
+      type: `type text 5`
+    },
+  ]
+};
 
-it(`Render App correctly`, () => {
+test(`Render App correctly`, () => {
+  const {offers} = mock;
   const tree = rerender.create(
-      <App offersCount={3} titlesOffers={titlesOffers}/>)
+      <App
+        offersCount={3}
+        offers={offers}
+        onTitleOfferClick={() => {}}
+      />)
       .toJSON();
   expect(tree).toMatchSnapshot();
 });
---
+++ src/components/offer-card/offer-card.e2e.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import OfferCard from "./offer-card";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({adapter: new Adapter()});
+
+const mock = {
+  offer: {
+    id: Date.now() + Math.random(),
+    title: ``,
+    mark: ``,
+    previewImage: ``,
+    price: 1,
+    bookmark: false,
+    rating: 1,
+    type: ``
+  }
+};
+
+test(`Mouseover event over offer card pass data about this offer to handler`, () => {
+  const {offer} = mock;
+  const onCardMouseOver = jest.fn();
+
+  const wrapper = shallow(<OfferCard
+    onTitleOfferClick={() => {}}
+    onCardMouseOver={onCardMouseOver}
+    offer={offer}/>);
+
+  wrapper.simulate(`mouseover`);
+
+  expect(onCardMouseOver.mock.calls[0][0]).toMatchObject(offer);
+});
+
+test(`Should link of title be clicked`, () => {
+  const {offer} = mock;
+  const onTitleOfferClick = jest.fn();
+
+  const wrapper = shallow(<OfferCard
+    onTitleOfferClick={onTitleOfferClick}
+    onCardMouseOver={() => {}}
+    offer={offer}/>);
+
+  const titleLink = wrapper.find(`.place-card__name > a`);
+  titleLink.simulate(`click`);
+  expect(onTitleOfferClick).toHaveBeenCalledTimes(1);
+});
