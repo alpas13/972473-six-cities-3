61acbb3d3e5fc8ce8881e711fe89d672b053d043
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-02-18 19:32:17 +0100

    Update mock data in OfferCard component

--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -10,7 +10,7 @@ const OfferCard = (props) => {
     }}>
       {offer.mark ? <div className="place-card__mark">
         <span>{offer.mark}</span>
-      </div> : ``}
+      </div> : null}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
           <img className="place-card__image" src={offer.previewImage} width="260" height="200" alt="Place image"/>
@@ -22,7 +22,7 @@ const OfferCard = (props) => {
             <b className="place-card__price-value">&euro;{offer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className={`place-card__bookmark-button ${offer.bookmark ? `place-card__bookmark-button--active` : ``} button`} type="button">
+          <button className={`place-card__bookmark-button ${offer.bookmark ? `place-card__bookmark-button--active` : null} button`} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark" />
             </svg>
@@ -31,7 +31,7 @@ const OfferCard = (props) => {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: `${offer.rating}%`}} />
+            <span style={{width: `${offer.rating[`star`]}%`}} />
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
@@ -40,7 +40,7 @@ const OfferCard = (props) => {
             onTitleOfferClick(offer);
           }}>{offer.title}</a>
         </h2>
-        <p className="place-card__type">{offer.type}</p>
+        <p className="place-card__type">{offer.features[`entire`]}</p>
       </div>
     </article>
   );
@@ -49,16 +49,15 @@ const OfferCard = (props) => {
 OfferCard.propTypes = {
   onTitleOfferClick: PropTypes.func.isRequired,
   onCardMouseOver: PropTypes.func.isRequired,
-  offer: PropTypes.shape({
+  offer: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    mark: PropTypes.string,
-    previewImage: PropTypes.string.isRequired,
+    mark: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
-    bookmark: PropTypes.boolean,
-    rating: PropTypes.number.isRequired,
-    type: PropTypes.string.isRequired,
-  }).isRequired
+    bookmark: PropTypes.string.isRequired,
+    rating: PropTypes.object.isRequired,
+    features: PropTypes.object.isRequired,
+  })).isRequired
 };
 
 export default OfferCard;
