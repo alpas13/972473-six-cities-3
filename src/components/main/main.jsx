8a3d71ae0c721e353c61b74bea34fb4f05790e8f
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-02-22 11:56:22 +0100

    Section cities__places moved from Main component to OffersList component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Map from "../map/map.jsx";
 
 
 const Main = (props) => {
-  const {offersCount, offers, children} = props;
+  const {children} = props;
 
   return (
     <main className="page__main page__main--index">
@@ -47,44 +46,7 @@ const Main = (props) => {
       </div>
       <div className="cities">
         <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{offersCount} places to stay in Amsterdam</b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                <svg className="places__sorting-arrow" width="7" height="4">
-                  <use xlinkHref="#icon-arrow-select" />
-                </svg>
-              </span>
-              <ul className="places__options places__options--custom places__options--opened">
-                <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                <li className="places__option" tabIndex="0">Price: low to
-                        high
-                </li>
-                <li className="places__option" tabIndex="0">Price: high to
-                        low
-                </li>
-                <li className="places__option" tabIndex="0">Top rated first
-                </li>
-              </ul>
-              <select className="places__sorting-type" id="places-sorting">
-                <option className="places__option" value="popular" defaultValue>Popular</option>
-                <option className="places__option" value="to-high">Price: low to high</option>
-                <option className="places__option" value="to-low">Price: high to low</option>
-                <option className="places__option" value="top-rated">Top rated first</option>
-              </select>
-            </form>
-            <div className="cities__places-list places__list tabs__content">
-              {children}
-            </div>
-          </section>
-          <div className="cities__right-section">
-            <section className="cities__map map">
-              <Map offers={offers}/>
-            </section>
-          </div>
+          {children}
         </div>
       </div>
     </main>
@@ -93,7 +55,6 @@ const Main = (props) => {
 
 Main.propTypes = {
   children: PropTypes.node.isRequired,
-  offersCount: PropTypes.number.isRequired,
 };
 
 export default Main;
--- src/components/offers-list/offers-list.jsx
+++ src/components/offers-list/offers-list.jsx
@@ -24,23 +24,57 @@ class OffersList extends PureComponent {
   }
 
   render() {
-    const {offers, onTitleOfferClick} = this.props;
+    const {offers, offersCount, city, onTitleOfferClick} = this.props;
 
     return (
-      offers.map((offer) => {
-        const id = this._idGenerator();
-        return <OfferCard key={id}
-          onTitleOfferClick={onTitleOfferClick}
-          onCardMouseOver={this._handleCardMouseOver}
-          offer={offer}
-        />;
-      })
+      <section className="cities__places places">
+        <h2 className="visually-hidden">Places</h2>
+        <b className="places__found">{offersCount} places to stay in {city}</b>
+        <form className="places__sorting" action="#" method="get">
+          <span className="places__sorting-caption">Sort by</span>
+          <span className="places__sorting-type" tabIndex="0">
+                Popular
+            <svg className="places__sorting-arrow" width="7" height="4">
+              <use xlinkHref="#icon-arrow-select" />
+            </svg>
+          </span>
+          <ul className="places__options places__options--custom places__options--opened">
+            <li className="places__option places__option--active" tabIndex="0">Popular</li>
+            <li className="places__option" tabIndex="0">Price: low to
+              high
+            </li>
+            <li className="places__option" tabIndex="0">Price: high to
+              low
+            </li>
+            <li className="places__option" tabIndex="0">Top rated first
+            </li>
+          </ul>
+          <select className="places__sorting-type" id="places-sorting">
+            <option className="places__option" value="popular" defaultValue>Popular</option>
+            <option className="places__option" value="to-high">Price: low to high</option>
+            <option className="places__option" value="to-low">Price: high to low</option>
+            <option className="places__option" value="top-rated">Top rated first</option>
+          </select>
+        </form>
+        <div className="cities__places-list places__list tabs__content">
+          {offers.map((offer) => {
+            const id = this._idGenerator();
+            return <OfferCard key={id}
+              onTitleOfferClick={onTitleOfferClick}
+              onCardMouseOver={this._handleCardMouseOver}
+              offer={offer}
+            />;
+          })}
+        </div>
+      </section>
     );
   }
 }
 
 OffersList.propTypes = {
   offers: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  offersCount: PropTypes.number.isRequired,
+  city: PropTypes.string.isRequired,
   onTitleOfferClick: PropTypes.func.isRequired,
 };
 
