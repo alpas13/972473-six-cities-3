40a5395db3b1245bee950650af51592867e0b270
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-03-19 14:26:07 +0100

    Described actions in user's reduce and added checking authorization status to index

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import thunk from "redux-thunk";
 import {Provider} from "react-redux";
 import reducer from "./reducer/reducer.js";
 import {Operation as DataOperation} from "./reducer/data/data.js";
+import {ActionCreator, AuthorizationStatus, Operation as UserOperation} from "./reducer/user/user.js";
 import App from "./components/app/app.jsx";
 import {createApi} from "./api.js";
 
-const api = createApi();
+const onUnauthorized = () => {
+  store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createApi(onUnauthorized);
 
 const store = createStore(
     reducer,
@@ -19,6 +24,7 @@ const store = createStore(
 );
 
 store.dispatch(DataOperation.loadData());
+store.dispatch(UserOperation.checkAuth());
 
 ReactDom.render(
     <Provider store={store}>
--- src/reducer/user/user.js
+++ src/reducer/user/user.js
@@ -32,4 +32,26 @@ const reducer = (state = initialState, action) => {
   return state;
 };
 
-export {reducer, ActionType, ActionCreator};
+const Operation = {
+  checkAuth: () => (dispatch, getState, api) => {
+    return api.get(`/login`)
+        .then(() => {
+          dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+        })
+        .catch((err) => {
+          throw err;
+        });
+  },
+
+  login: (authData) => (dispatch, getState, api) => {
+    return api.post(`/login`, {
+      email: authData.login,
+      password: authData.password,
+    })
+        .then(() => {
+          dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+        });
+  },
+};
+
+export {reducer, ActionType, ActionCreator, AuthorizationStatus, Operation};
