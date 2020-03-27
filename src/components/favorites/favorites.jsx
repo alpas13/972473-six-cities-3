ca2ad729e6972a5017b2ebb48aac6523d7421ccf
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-03-27 13:24:02 +0100

    Fixed changing pages between favorites page and empty favorites page

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -6,9 +6,10 @@ import {uniqueFilter, filterByValue} from "../../utils";
 import {favoritesStyle} from "../../const";
 import {connect} from "react-redux";
 import {getFavorites} from "../../reducer/main/selectors";
+import {ActionCreator} from "../../reducer/main/main";
 
 const Favorites = React.memo(function Favorites(props) {
-  const {offers} = props;
+  const {offers, getFavoritesPage} = props;
   return (
     <div className="page">
       <Header />
@@ -17,25 +18,26 @@ const Favorites = React.memo(function Favorites(props) {
           <section className="favorites">
             <h1 className="favorites__title">Saved listing</h1>
             <ul className="favorites__list">
-              {uniqueFilter(offers, `city`).map((city) => {
-                return (
-                  <li key={city} className="favorites__locations-items">
-                    <div className="favorites__locations locations locations--current">
-                      <div className="locations__item">
-                        <a className="locations__item-link" href="#">
-                          <span>{city}</span>
-                        </a>
+              {offers.length === 0 ? getFavoritesPage(false) :
+                uniqueFilter(offers, `city`).map((city) => {
+                  return (
+                    <li key={city} className="favorites__locations-items">
+                      <div className="favorites__locations locations locations--current">
+                        <div className="locations__item">
+                          <a className="locations__item-link" href="#">
+                            <span>{city}</span>
+                          </a>
+                        </div>
                       </div>
-                    </div>
-                    <div className="favorites__places">
-                      <OffersList
-                        offers={filterByValue(offers, `city`, city)}
-                        styleSettings={favoritesStyle}
-                      />
-                    </div>
-                  </li>
-                );
-              })}
+                      <div className="favorites__places">
+                        <OffersList
+                          offers={filterByValue(offers, `city`, city)}
+                          styleSettings={favoritesStyle}
+                        />
+                      </div>
+                    </li>
+                  );
+                })}
             </ul>
           </section>
         </div>
@@ -51,11 +53,18 @@ const Favorites = React.memo(function Favorites(props) {
 
 Favorites.propTypes = {
   offers: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  getFavoritesPage: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   offers: getFavorites(state),
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  getFavoritesPage(status) {
+    dispatch(ActionCreator.favoritesPage(status));
+  }
+});
+
 export {Favorites};
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
