7160c4e0d25bca7e46c503a54d7906429bdbb968
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-03-29 18:30:37 +0200

    Added test for ReviewForm

---
+++ src/components/review-form/review-form.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import ReviewForm from "./review-form.jsx";
+
+test(`Correctly render ReviewItem component`, () => {
+  const tree = renderer.create(
+      <ReviewForm
+        rating={``}
+        comment={``}
+        status={true}
+        handleChangeRating={() => {}}
+        handleChangeTextarea={() => {}}
+        handleSubmit={() => {}}
+      />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
