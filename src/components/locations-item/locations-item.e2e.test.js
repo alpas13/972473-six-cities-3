fc94101588e2d10073199d65e8277a08c71e3ec4
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-04-04 15:03:45 +0200

    Added e2e test to LocationItem component

---
+++ src/components/locations-item/locations-item.e2e.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import LocationsItem from "./locations-item";
+import {configure, mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({adapter: new Adapter()});
+
+test(`Click event on city pass value of city to handler`, () => {
+  const changeCity = jest.fn();
+
+  const wrapper = mount(
+      <LocationsItem
+        activeItem={`Hamburg`}
+        cityItem={`Amsterdam`}
+        onChange={changeCity}
+      />);
+
+  wrapper.find(`.locations__item`).simulate(`click`, {
+    target: {tagName: `SPAN`}
+  });
+
+  expect(changeCity).toHaveBeenCalledWith(`Amsterdam`);
+});
